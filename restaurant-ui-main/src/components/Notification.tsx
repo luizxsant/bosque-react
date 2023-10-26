33131edeb52052e570f9b187492a190fabb5280a
luizxsant <124802162+luizxsant@users.noreply.github.com>
2023-10-26 12:04:40 -0300

    teste commit

--- restaurant-ui-main/src/components/Notification.tsx
+++ restaurant-ui-main/src/components/Notification.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notification = () => {
   return (
-    <div className='h-12 bg-pad text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Entregas somente em Barão Geraldo!
+    <div className='h-12 bg-pad text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Entregas somente em Barão Campinas!
     </div>
   )
 }
